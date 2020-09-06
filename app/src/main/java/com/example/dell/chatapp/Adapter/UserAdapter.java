297984c50f60c6450806889a84eb0ded6f55d653
Nassima ZIANI <nassima.ziani1996@gmail.com>
2020-09-06 14:58:01 +0200

    06/09/2020 15h00
    did the following:
    - created a new method 'lastMessage' in UserAdapter
    - get the last message exchanged between the current user and the user we communication with
    - display the last message
    
    status:
    - tested
    - works perfect
    - find 0 error

--- app/src/main/java/com/example/dell/chatapp/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/dell/chatapp/Adapter/UserAdapter.java
@@ -2,6 +2,7 @@ package com.example.dell.chatapp.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,16 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.dell.chatapp.MessageActivity;
+import com.example.dell.chatapp.Model.Chat;
 import com.example.dell.chatapp.Model.User;
 import com.example.dell.chatapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -26,6 +35,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     private Context mContext;
     private List<User> mUsers;
     private boolean ischat;
+
+    String theLastMessage;
 
     // constructor
     public UserAdapter(Context mContext, List<User> mUsers, boolean ischat) {
@@ -55,6 +66,19 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             holder.profile_img.setImageResource(R.mipmap.ic_launcher);
         } else {
             Glide.with(mContext).load(user.getImgURL()).into(holder.profile_img);
+        }
+
+        // check if ischat is true
+        if (ischat) {
+
+            // call the method
+            lastMessage(user.getId(), holder.last_msg);
+
+        } else {
+
+            // don't display the textView
+            holder.last_msg.setVisibility(View.GONE);
+
         }
 
         // check if ischat is true
@@ -101,6 +125,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         public ImageView profile_img;
         private ImageView img_on;
         private ImageView img_off;
+        private TextView last_msg;
 
         // constructor
         public ViewHolder(View itemView) {
@@ -110,7 +135,69 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             profile_img = itemView.findViewById(R.id.profile_img);
             img_on = itemView.findViewById(R.id.img_on);
             img_off = itemView.findViewById(R.id.img_off);
+            last_msg = itemView.findViewById(R.id.last_msg);
 
         }
+    }
+
+    // check for last message
+
+    /**
+     * this method allow to check for the last message and display it
+     * @param userid : the id of the user we chatting with
+     * @param last_msg : the TextView where the last message will be displayed
+     */
+    private  void lastMessage(final String userid, final TextView last_msg) {
+
+        // set last message to default
+        theLastMessage = "default";
+
+        // get the current user
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        // get an instance of the Chats
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Chats");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                // for each chat
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    // fetch the chat
+                    Chat chat = snapshot.getValue(Chat.class);
+
+                    // check if there is a message exchanged between the current user and the user we communicating with
+                    if (chat.getReceiver().equals(firebaseUser.getUid()) && chat.getSender().equals(userid) ||
+                            chat.getReceiver().equals(userid) && chat.getSender().equals(firebaseUser.getUid())) {
+
+                        // get the message
+                        theLastMessage = chat.getMessage();
+
+                    }
+                }
+
+                // check if the lastMessage is default (the 2 never communicated)
+                if (theLastMessage.equals("default")) {
+
+                    // display 'aucun message'
+                    last_msg.setText("Aucun message");
+
+                } else {
+
+                    // display the last message exchanged
+                    last_msg.setText(theLastMessage);
+
+                }
+
+                theLastMessage = "default";
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
